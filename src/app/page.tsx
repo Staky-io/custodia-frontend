7a64e619ebf99b196b96eca1e89782fdf57ac4af
aymxdd <aymxdd@gmail.com>
2023-07-11 17:54:40 +0200

    feat(ui): add inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
-import { Button, Heading, Text } from '~/components/common'
+import { useState } from 'react'
+import { Button, Heading, Input, Text } from '~/components/common'
 
 export default function Home() {
+    const [inputValue, setInputValue] = useState('')
+    const [inputValue2, setInputValue2] = useState('')
+
     return (
         <main>
             <Text>Hello world</Text>
@@ -12,10 +16,26 @@ export default function Home() {
             <Heading level={4}>Heading 4</Heading>
             <Heading level={5}>Heading 5</Heading>
             <Heading level={6}>Heading 6</Heading>
-            <Button
-                variant="primary"
-                onClick={() => console.log('hello')}
-            >Button</Button>
+
+            <div className='flex flex-row mt-128'>
+                <Button variant="primary" className='mr-10'>Button primary</Button>
+                <Button variant="secondary">Button secondary</Button>
+            </div>
+
+            <Input
+                value={inputValue}
+                label={false}
+                placeholder='Classic placeholder'
+                className='mt-60'
+                onChange={(value) => setInputValue(value)}
+            />
+            <Input
+                value={inputValue2}
+                label={true}
+                placeholder='Placeholder with label'
+                className='mt-10'
+                onChange={(value) => setInputValue2(value)}
+            />
         </main>
     )
 }
